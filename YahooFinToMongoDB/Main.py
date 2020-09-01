ec4688bfd78fb09cb32cfcafa565274e91f63bd2
producode <produgraf2000@gmail.com>
2020-09-01 18:58:29 -0300

    Se arreglo el problema del reshape

--- YahooFinToMongoDB/Main.py
+++ YahooFinToMongoDB/Main.py
@@ -10,11 +10,14 @@ import pickle
 from keras.utils import np_utils
 import tensorflow as tf
 
+
 def getTokenBullMarket(dni,password):
     url = "https://bullmarketbrokers.com/Home/Login"
 
-    payload = {'idNumber': str(dni),
-    'password': password}
+    payload = {
+        'idNumber': str(dni),
+        'password': password
+    }
 
     headers = {
       'Cookie': 'ASP.NET_SessionId=et443mc4ngbnfe5suw541bhf; 7572a1cd-608b-45e4-b6a7-fdb52f674e8c=UserId=UJhud7cd59jbcDYUz0dLvw==; lastUserIdLogged=lastUserIdLogged=113203; notificationCookie=notifications={"type1":[],"type2":[]}'
@@ -25,9 +28,10 @@ def getTokenBullMarket(dni,password):
     response = response.text.encode('utf-8')
     response = json.loads(response)
 
-    if(response['succeed']):
+    if response['succeed']:
         return response['token']
     return 'Failure'
+
 
 def getCedearsFrom(tickets):
     ticketsAvaliables = []
@@ -39,6 +43,7 @@ def getCedearsFrom(tickets):
         except:
             continue
     return ticketsAvaliables
+
 
 def updateTicketsCedears(mongoClientUsed):
     cedears = []
@@ -71,32 +76,56 @@ def updateTicketsCedears(mongoClientUsed):
         mydict = { "TicketName": ticket}
         x = mycol.insert_one(mydict)
 
-def getInfoFewDaysAgo(days,actualDate,ticket):
+
+def getInfoFewDaysAgo(days, actualDate, ticket):
     date_N_days_ago = actualDate - timedelta(days=days)
     dateInStringStart = str(date_N_days_ago.month) + "/" + str(date_N_days_ago.day) + "/" + str(date_N_days_ago.year)
     dateInStringEnd = str(actualDate.month) + "/" + str(actualDate.day) + "/" + str(actualDate.year)
-    print("getInfoFewDaysAgo: " + str(days))
 
     return get_data(ticket, start_date=dateInStringStart,end_date=dateInStringEnd)
 
-def getBollingerBands(days,actualDate,ticket):
+
+def getBollingerBands(days, actualDate, ticket):
     data = getInfoFewDaysAgo(days, actualDate, ticket)
     standarsDesviations = []
-    aux = data.loc[:, ['open', 'close', 'high', 'low']]
     totalDays = data['open'].to_numpy().__len__()
     dataPastTest = getInfoFewDaysAgo(days*2, actualDate, ticket)
     for day in range(totalDays):
         dataPast = dataPastTest.iloc[day:totalDays+day]
         promediosPast = dataPast.loc[:, ['open', 'close', 'high', 'low']]
         standarsDesviations.append(np.std(promediosPast.mean(1).to_numpy()))
+    aux = data.loc[:, ['open', 'close', 'high', 'low']]
     BollingerBand = {
         'prom': aux.mean(1).to_numpy(),
         "standarDesviation": standarsDesviations
     }
     return BollingerBand
 
-def acotation(max,min,num):
+
+def acotation(max, min, num):
+    if (max - min) < 1:
+        return int(((num - min) / 1) * 99)
     return int(((num - min) / (max - min)) * 99)
+
+
+def comparation(rangeEquality, firstNumber, secondNumber):
+    if secondNumber > firstNumber - 5 and secondNumber < firstNumber:
+        return 3
+    elif secondNumber > firstNumber - 10 and secondNumber <= firstNumber - 5:
+        return 2
+    elif secondNumber > firstNumber - 20 and secondNumber <= firstNumber - 10:
+        return 1
+    elif secondNumber <= firstNumber - 20:
+        return 0
+
+    if secondNumber < firstNumber + 5 and secondNumber >= firstNumber:
+        return 4
+    elif secondNumber < firstNumber + 10 and secondNumber >= firstNumber + 5:
+        return 5
+    elif secondNumber < firstNumber + 20 and secondNumber >= firstNumber + 10:
+        return 6
+    elif secondNumber >= firstNumber + 20:
+        return 7
 
 
 def createTrainsDataCedears(dateToStart):
@@ -111,45 +140,54 @@ def createTrainsDataCedears(dateToStart):
     for cedear in mycol.find():
         cedears.append(cedear['TicketName'])
     trainOrTest = True
-    for NroCedear in tqdm(range(2)):
-        cedearTicket = cedears[NroCedear]
-        prom = (getInfoFewDaysAgo(1, date_time + timedelta(days=1), cedearTicket))
-        prom = [prom["open"][0], prom["close"][0], prom["high"][0], prom["low"][0]]
-        prom = np.mean(prom)
-        print(DAYS)
-        data = getInfoFewDaysAgo(DAYS, date_time, cedearTicket)
-        Bands = getBollingerBands(DAYS, date_time, cedearTicket)
-        xQuantity = len(Bands['prom'])
-        heatMap = np.zeros((100, xQuantity))
-        for xPos in range(xQuantity):
-            yPos = acotation(np.max(data['high']), np.min(data['low']), Bands['prom'][xPos])
-            yPosClose = acotation(np.max(data['high']), np.min(data['low']), data['close'][xPos])
-            yPosOpen = acotation(np.max(data['high']), np.min(data['low']), data['open'][xPos])
+    for NroCedear in tqdm(range(20)):
+        try:
+            cedearTicket = cedears[NroCedear]
+            prom = (getInfoFewDaysAgo(1, date_time + timedelta(days=1), cedearTicket))
+            prom = [prom["open"][0], prom["close"][0], prom["high"][0], prom["low"][0]]
+            prom = np.mean(prom)
+            data = getInfoFewDaysAgo(DAYS, date_time, cedearTicket)
+            Bands = getBollingerBands(DAYS, date_time, cedearTicket)
+            xQuantity = len(Bands['prom'])
+            heatMap = np.zeros((100, xQuantity))
+            for xPos in range(xQuantity):
+                yPos = acotation(np.max(data['high']), np.min(data['low']), Bands['prom'][xPos])
+                yPosClose = acotation(np.max(data['high']), np.min(data['low']), data['close'][xPos])
+                yPosOpen = acotation(np.max(data['high']), np.min(data['low']), data['open'][xPos])
+                if yPos >= 100:
+                    yPos = 99
+                if yPosClose >= 100:
+                    yPosClose = 99
+                if yPosOpen >= 100:
+                    yPosOpen = 99
 
-            if (yPosOpen < yPosClose):
-                heat = 100
-                plus = 80 / (yPosClose - yPosOpen)
-                for y in range(yPosOpen, yPosClose):
-                    heatMap[y][xPos] = int(heat)
-                    heat += plus
-            elif (yPosClose < yPosOpen):
-                heat = 180
-                substrac = 80 / (yPosOpen - yPosClose)
-                for y in range(yPosClose, yPosOpen):
-                    heatMap[y][xPos] = int(heat)
-                    heat -= substrac
-            heatMap[yPos][xPos] = 255
-        if trainOrTest:
-            yTrains.append(acotation(np.max(data["high"]), np.min(data["low"]), prom))
-            xTrains.append(heatMap.copy())
-        else:
-            yTest.append(acotation(np.max(data["high"]), np.min(data["low"]), prom))
-            xTest.append(heatMap.copy())
-        trainOrTest = not trainOrTest
-    yTrains = np.array(yTrains)
+                if yPosOpen < yPosClose:
+                    heat = 100
+                    plus = 80 / (yPosClose - yPosOpen)
+                    for y in range(yPosOpen, yPosClose):
+                        heatMap[y][xPos] = int(heat)
+                        heat += plus
+                elif yPosClose < yPosOpen:
+                    heat = 180
+                    substrac = 80 / (yPosOpen - yPosClose)
+                    for y in range(yPosClose, yPosOpen):
+                        heatMap[y][xPos] = int(heat)
+                        heat -= substrac
+                heatMap[yPos][xPos] = 255
+
+            if trainOrTest:
+                yTrains.append(acotation(np.max(data["high"]), np.min(data["low"]), prom))
+                xTrains.append(heatMap.copy())
+            else:
+                yTest.append(acotation(np.max(data["high"]), np.min(data["low"]), prom))
+                xTest.append(heatMap.copy())
+            trainOrTest = not trainOrTest
+        except:
+            continue
     xTrains = np.array(xTrains)
-    yTest = np.array(yTest)
+    yTrains = np.array(yTrains)
     xTest = np.array(xTest)
+    yTest = np.array(yTest)
 
     return xTrains, yTrains, xTest, yTest
 
@@ -161,7 +199,9 @@ mydb = myclient["Market"]
 mycol = mydb["ticketsCedears"]
 myData = mydb["cedearData"]
 
-#get de heatMap (just 2 cedears for now)
+# updateTicketsCedears("mongodb://localhost:27017/")
+
+# get de heatMap (just 2 cedears for now)
 xTrains, yTrains, xTest, yTest = createTrainsDataCedears("2020-08-26")
 
 """
@@ -175,32 +215,35 @@ for mapAndResult in range(len(xTrains)):
     x = myData.insert_one(mydict)
 """
 
-#adapt the data
+# adapt the data
 
-xTrains.reshape(xTrains.shape[0], xTrains.shape[1]*xTrains.shape[2])
-xTest.reshape(xTest.shape[0], xTest.shape[1]*xTest.shape[2])
 
-xTrains = xTrains/100
-xTest = xTest/100
+xTrains = xTrains.reshape((xTrains.shape[0], xTrains.shape[1]*xTrains.shape[2]))
+xTest = xTest.reshape((xTest.shape[0], xTest.shape[1]*xTest.shape[2]))
+
+xTrains = xTrains/255
+xTest = xTest/255
 
 yTrains = np_utils.to_categorical(yTrains, 100)
 yTest = np_utils.to_categorical(yTest, 100)
 
-#create the model
+# create the model
+
 
 class MyModel(tf.keras.Model):
 
-  def __init__(self):
-    super(MyModel, self).__init__()
-    self.dense1 = tf.keras.layers.Dense(32, input_dim=2100, activation=tf.nn.relu)
-    self.dense2 = tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-    self.dropout = tf.keras.layers.Dropout(0.5)
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.dense1 = tf.keras.layers.Dense(32, input_dim=2100, activation=tf.nn.relu)
+        self.dense2 = tf.keras.layers.Dense(100, activation=tf.nn.softmax)
+        self.dropout = tf.keras.layers.Dropout(0.5)
 
-  def call(self, inputs):
-      x = self.dense1(inputs)
-      return self.dense2(x)
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        return self.dense2(x)
 
-#use the model
+# use the model
+
 
 model = MyModel()
 
@@ -208,7 +251,10 @@ model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accura
 
 model.fit(x=xTrains, y=yTrains, batch_size=100, epochs=10, verbose=1, validation_data=(xTest, yTest))
 
-model.evaluate(x=xTest, y=yTest, batch_size=100, verbose=1)
+evaluacion = model.evaluate(x=xTest, y=yTest, batch_size=100, verbose=1)
+
+print(yTest)
+print(evaluacion)
 
 
 """
@@ -234,7 +280,7 @@ updateTicketsCedears(mongoClientUsed)
 Description: 
 Update the name of cedears's tickets in the database
 ------------------------------------------------
-getCedearsFrom(tickets) 
+getCedearsFrom(tickets)
 
 Description:
 check in an array if it have valids cedears and give you the truly tickets
